29ce84ea65825e37964822e105534c7cf773870a
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-18 15:56:51 -0400

    Compiling Javascript from TypeScript

---
+++ with-typescript.js
@@ -0,0 +1,5 @@
+function addNubmers(a, b) {
+  return a + b;
+}
+var result2 = addNubmers(2, 5);
+console.log(result2);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+function addNubmers(a: number, b: number) {
+  return a + b;
+}
+
+const result2 = addNubmers(2, 5);
+
+console.log(result2);
