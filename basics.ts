93501e55de70c0eb7655f51f7f7d56195ca96d42
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-18 16:01:14 -0400

    Base Types and Primitives

---
+++ basics.ts
@@ -0,0 +1,16 @@
+// Primitives: number, dtring, boolean, string
+// More Complex types: arrays, objects, strings
+// Function types. parameters: number
+
+// Primitives
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = "Isaac";
+
+let isInstructor: boolean;
+
+isInstructor = false;
