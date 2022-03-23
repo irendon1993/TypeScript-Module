7f19517f9929fc363fcd0687b09d6b0280e6a2d7
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-22 21:12:46 -0400

    Array and Object types

--- basics.ts
+++ basics.ts
@@ -14,3 +14,29 @@ userName = "Isaac";
 let isInstructor: boolean;
 
 isInstructor = false;
+
+// More Complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Isaac",
+  age: 28,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+// Array of objects(persons)
+let people: {
+  name: string;
+  age: number;
+}[];
